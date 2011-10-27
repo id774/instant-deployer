bd2255de7deae5c9c6befd5cb84407bab28c632e
id774 <idnanashi@gmail.com>
2011-10-27 10:30:29 +0900

    1 Skelton

---
+++ bin/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+setup() {
+    test -n "$1" || CONFIG=config/deploy.conf
+    test -n "$1" && CONFIG=$1
+    test "$1" = "--test" && CONFIG=test/test.conf
+    dir_name() { echo ${1%/*}; }
+    SCRIPT_HOME=$(cd $(dir_name $0) && pwd)/..
+    . $SCRIPT_HOME/$CONFIG
+}
+
+send_mail_to_admin() {
+    nkf -w $JOBLOG | \
+      mail -s "[admin-log][`/bin/hostname`] Instant Deploy Log" \
+      $ADMIN_MAIL_ADDRESS
+}
+
+instant_deployer() {
+    echo -n "*** $0: Job start at `/bin/hostname` on ">>$JOBLOG 2>&1
+    date "+%Y/%m/%d %T">>$JOBLOG 2>&1
+    . $SCRIPT_HOME/lib/loader.sh>>$JOBLOG 2>&1
+    echo -n "*** $0: End of Job at `/bin/hostname` on ">>$JOBLOG 2>&1
+    date "+%Y/%m/%d %T">>$JOBLOG 2>&1
+    echo>>$JOBLOG 2>&1
+    case "$ADMIN_MAIL_ADDRESS" in
+      *@*)
+        send_mail_to_admin
+        ;;
+    esac
+}
+
+main() {
+    setup $*
+    instant_deployer
+}
+
+main $*
